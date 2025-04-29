0058ed069b6d69e7372e6af09f0223a0848c7359
yvialko <ekl301289@gmail.com>
2025-04-29 13:49:59 +0300

    HW++

---
+++ homework_lesson_10_1.js
@@ -0,0 +1,69 @@
+var arr = [
+    {
+        userName: "Test",
+        lastName: "Test",
+        email: "test.test@gmail.com"
+    },
+    {
+        userName: "Dmitro",
+        lastName: "Porohov",
+        "<email": "dmitro.porohov@yahoo.com>"
+    },
+    {
+        userName: "Andrii",
+        lastName: "",
+        email: "andrii@mail.ru" // Нам такі не підходять
+    },
+    {
+        userName: "test",
+        lastName: "test",
+        "&email": "test@yahoo.com"
+    },
+    {
+        userName: "test2",
+        lastName: "test2",
+        emai: "test2@yahoo.com"
+    }
+];
+
+let allowedEmails = [];
+//let notAllowedEmail = [];
+
+arr.forEach(function(persons){
+                                function checkEmail(key) {
+                                    return key.includes("email");
+                                    //console.log(key.includes("email"))
+                                }
+
+                                let emailKey = Object.keys(persons).find(checkEmail)
+                                //console.log(Object.keys(persons))
+
+                                if (emailKey) {
+                                    let email = persons[emailKey].replace(/[<>!?%&+_]/g, "").trim();
+
+                                    if (/^[a-zA-Z0-9.-_]+@(gmail\.com|yahoo\.com)$/.test(email)) {
+                                        allowedEmails.push(email);
+                                    }  /*else {
+                                                    notAllowedEmail.push(email)
+                                                }*/
+                                }
+});
+
+console.log("all allowed emails:", allowedEmails);
+//console.log("all restricted emails:", notAllowedEmail);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let test1 = "  <email";
+// console.log(test1.replace(/[<>!?%&+_]/gm, ""));
